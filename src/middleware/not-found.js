39a93ef65570a57f30d4578722c9ed943fc6f7fe
mickael <richardmickael1991@gmail.com>
2022-01-23 19:56:44 +0100

    app deployed

--- src/middleware/not-found.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFoundMiddleware = (req, res) => res.status(404).send("Route does not exist");
-exports.default = notFoundMiddleware;
